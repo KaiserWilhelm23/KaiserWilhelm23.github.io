70c18ddcdd31b2ab474ccb59f0eeeb000456ec69
Will Payne <67875237+blaze005@users.noreply.github.com>
2023-08-03 18:05:41 -0400

    Update script.js

--- xvibration/script.js
+++ xvibration/script.js
@@ -51,7 +51,7 @@ function updateGamepadStatus() {
     document.getElementById('stop-vibration-button').disabled = false;
   } else {
     gamepad = null;
-    document.getElementById('controller-status').textContent = 'Controller disconnected. Press "A" on the controller to connect. Or check if it is even connected to your device... ';
+    document.getElementById('controller-status').textContent = 'Controller disconnected. 1. Check if controller is connected to your device   2. Press any button on the controller to omplete connection. ';
     document.getElementById('stop-vibration-button').disabled = true;
   }
 }
